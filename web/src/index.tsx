52bba403b1053b0ea6f6125ad13f10f276cc82d6
Agustin Mulet <agustin.mulet@gmail.com>
2021-05-18 23:10:35 -0300

    Add and connect Apollo Client

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 import { ChakraProvider, theme, ColorModeScript } from "@chakra-ui/react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <ColorModeScript />
     <ChakraProvider theme={theme}>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
